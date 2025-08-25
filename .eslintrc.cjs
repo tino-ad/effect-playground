708491c070815022acbd6bce12b2f004756df39e
Tino ad <tino.goetz@adesso.de>
2025-08-25 16:37:37 +0200

    chore: add ESLint + Prettier config and scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist/", "node_modules/"],
+  rules: {},
+}
